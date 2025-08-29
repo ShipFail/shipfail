93b489b125669b79c49f99817fde8133669c8083
Luke Hollis <lukehollis@gmail.com>
2025-08-28 22:58:51 -0400

    Refactor Next.js config: remove unused options, add eslint setting.

--- next.config.js
+++ next.config.js
@@ -3,15 +3,12 @@
 /**@type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
   trailingSlash: true,
-  swcMinify: true,
   basePath: "",
-  assetPrefix:"",
-  images: {
-    loader: "imgix",
-    path: "/",
+  assetPrefix: "",
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
